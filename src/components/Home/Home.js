98ccd04be04d6066d6f64e6417750930f50f9cb9
4bhishekKasam <konnurabhishek@gmail.com>
2019-01-26 14:35:40 +0530

    movie searchbar added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,26 +6,33 @@ import {
   POSTER_SIZE,
   BACKDROP_SIZE
 } from "../../config/";
-import HeroImage from '../elements/HeroImage/HeroImage';
+import HeroImage from "../elements/HeroImage/HeroImage";
+import SearchBar from "../elements/SearchBar/SearchBar";
+import FourColGrid from "../elements/FourColGrid/FourColGrid";
+import MovieThumb from "../elements/MovieThumb/MovieThumb";
+import Spinner from "../elements/Spinner/Spinner";
 
 export default class Home extends Component {
-  state = {
-    movies: [],
-    heroImage: null,
-    loading: false,
-    currentPage: 0,
-    totalPages: 0,
-    searchTerm: ""
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      heroImage: null,
+      loading: false,
+      currentPage: 0,
+      totalPages: 0,
+      searchTerm: ""
+    };
+    this.searchItems = this.searchItems.bind(this);
+  }
   componentDidMount() {
     this.setState({ loading: true });
     const link = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
     this.fetchItems(link);
   }
 
-  fetchItems(link) {
-    fetch(link)
+  fetchItems(endpoint) {
+    fetch(endpoint)
       .then(res => res.json())
       .then(result =>
         this.setState(
@@ -56,6 +63,19 @@ export default class Home extends Component {
     this.fetchItems(link);
   }
 
+  searchItems(searchTerm) {
+    console.log(searchTerm);
+    let endpoint = "";
+    this.setState({ movies: [], loading: true, searchTerm });
+
+    if (searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+    this.fetchItems(endpoint);
+  }
+
   render() {
     return (
       <div className="rmdb-home">
@@ -68,8 +88,32 @@ export default class Home extends Component {
               title={this.state.heroImage.original_title}
               text={this.state.heroImage.overview}
             />
+            <SearchBar callback={this.searchItems} />
           </div>
         ) : null}
+        <div className="rmdb-home-grid">
+          <FourColGrid
+            header={this.state.searchTerm ? "Search Result" : "Popular Movies"}
+            loading={this.state.loading}
+          >
+            {this.state.movies.map((element, i) => {
+              return (
+                <MovieThumb
+                  key={i}
+                  clickable={true}
+                  image={
+                    element.poster_path
+                      ? `${IMAGE_BASE_URL}${POSTER_SIZE}/${element.poster_path}`
+                      : "./images/no_image.jpg"
+                  }
+                  movieId={element.id}
+                  movieName={element.original_title}
+                />
+              );
+            })}
+          </FourColGrid>
+          {this.state.loading ? <Spinner /> : null}
+        </div>
       </div>
     );
   }
